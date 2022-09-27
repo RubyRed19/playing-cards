f5c659e214d073d9d86bd129a3564f83fed0f7fe
RubyRed19 <mariaquintanamsis@gmail.com>
2022-09-27 13:26:55 -0700

    Added ColorFirst comparator.

--- src/main/java/com/tlglearning/cards/model/Deck.java
+++ src/main/java/com/tlglearning/cards/model/Deck.java
@@ -1,12 +1,15 @@
 package com.tlglearning.cards.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class Deck implements Iterable<Card>{
 
-  private final List<Card> cards;
+  private final List<Card> cards; //composition with cards
 
   //Constructor -no parameter👌
   public Deck() {
@@ -22,6 +25,50 @@ public class Deck implements Iterable<Card>{
 
   @Override
   public Iterator<Card> iterator() {
-    return cards.iterator();
+    return Collections.unmodifiableList(cards).iterator();
   }
+
+  public void shuffle(Random rng){
+    Collections.shuffle(cards, rng);
+  }
+
+  public void shuffle(){
+    Collections.shuffle(cards);
+  }
+
+  public void sort(){
+    sort(null);
+  }
+
+  public void sort(Comparator<Card> comparator){
+    cards.sort(comparator);
+  }
+
+  @Override
+  public int hashCode(){
+    return cards.hashCode();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean result;
+    if (this == obj) {
+      result = true;
+    } else if (obj instanceof Deck) {
+      result = cards.equals(((Deck) obj).cards);
+    } else {
+      result = false;
+    }
+    return result;
+  }
+
+  @Override
+  public String toString(){
+    return cards.toString();
+  }
+
 }
+
+
+
+
